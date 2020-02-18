2de9c29a6c0489c7ab2010c1012219cadf6e8a7d
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2020-02-18 18:32:47 +0600

    testclass

---
+++ TriangleTest.java
@@ -0,0 +1,12 @@
+package lab4.com;
+
+public class TriangleTest {
+
+	public static void main(String[] args) {
+		
+		Triangle T1 =new Triangle ( 4,5,6);
+		T1.Show_Info();
+		T1.Which_Triangle();
+	}
+
+}
