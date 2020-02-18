d60670979f084d81e97c0763b959220ecce14fb2
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2020-02-18 18:33:48 +0600

    Testclass

---
+++ AccountTest.java
@@ -0,0 +1,18 @@
+package lab4.com;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		Account a1 =new Account ();
+		a1.setAccName("nahid");
+	a1.setAcid("reflection");
+	a1.setBlance(500000);
+	a1.setDeposit(200000);
+	a1.setWithdraw(300000);
+	
+	a1.showinfo();
+	   System.out.println(a1);
+	  
+	}
+
+}
