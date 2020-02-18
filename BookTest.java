42a692ed051c423329456b056f6dc838f88e5043
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2020-02-18 18:34:43 +0600

    testclass

---
+++ BookTest.java
@@ -0,0 +1,17 @@
+package lab4.com;
+
+public class BookTest {
+
+	public static void main(String[] args) {
+		Book b1=new Book();
+		b1.setBookName("Paradoxical Sazid");
+		b1.setBookAuthor("Arif Azad");
+		b1.setBookcopy(5000000);
+		b1.setBooktyupe("Islamic Books");
+		b1.setBookid("tttttt");
+		b1.showinfo();
+		   System.out.println(b1);
+
+	}
+
+}
