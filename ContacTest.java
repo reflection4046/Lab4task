1e01b23be4b15ac1790574e4cd8669c4edab0ca9
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2020-02-18 18:54:30 +0600

    testclass

---
+++ ContacTest.java
@@ -0,0 +1,16 @@
+package lab4.com;
+
+public class ContacTest {
+
+	public static void main(String[] args) {
+		
+		   Contact C1 = new Contact ("Nahid", " 1223455 ", 20, "01311840020", "Male");
+		   
+	         System.out.println(C1);
+	         
+	         C1.ShowInfo();
+	         
+	         C1.detectOperator();
+	}
+
+}
